9df11eefc2ce8012df4dfd7442604b64803d6771
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-11-14 21:07:41 +0300

    Make DepartmentID nullable, add TodoItem and Vend entities
    Introduce migration to make DepartmentID in AspNetUsers nullable.
    Update BuildTargetModel and VendingContextModelSnapshot to reflect
    this change. Add new entities TodoItem and Vend with their properties
    and configurations. Set OnDelete behavior for DepartmentID foreign
    key in AppUser to Restrict without IsRequired constraint.

--- Vending.DataAccessLayer/Migrations/20241102124648_5_update_appuser_department_id_set_with_nullable.Designer.cs
+++ Vending.DataAccessLayer/Migrations/20241102124648_5_update_appuser_department_id_set_with_nullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vending.DataAccessLayer.Concrete;
 
@@ -11,9 +12,11 @@ using Vending.DataAccessLayer.Concrete;
 namespace Vending.DataAccessLayer.Migrations
 {
     [DbContext(typeof(VendingContext))]
-    partial class VendingContextModelSnapshot : ModelSnapshot
+    [Migration("20241102124648_5_update_appuser_department_id_set_with_nullable")]
+    partial class _5_update_appuser_department_id_set_with_nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -275,32 +278,6 @@ namespace Vending.DataAccessLayer.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("TodoItems");
-                });
-
-            modelBuilder.Entity("Vending.EntityLayer.Concrete.Vend", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<double>("Latitude")
-                        .HasColumnType("float");
-
-                    b.Property<string>("LocationDescription")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<double>("Longitude")
-                        .HasColumnType("float");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Vends");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<int>", b =>
---
+++ Vending.DataAccessLayer/Migrations/20241102124648_5_update_appuser_department_id_set_with_nullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vending.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _5_update_appuser_department_id_set_with_nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentID",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentID",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
