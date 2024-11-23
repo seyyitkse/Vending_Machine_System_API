2e42b0e87c11c311868e6411d7124fcfbf72f323
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-11-23 23:04:17 +0300

    Add AutoMapperConfig for DTO and entity mappings
    Introduce AutoMapperConfig class in Vending.ApiLayer.Mapping.
    Add necessary using directives for AutoMapper and project DTOs/entities.
    Define mappings in AutoMapperConfig constructor for various DTOs and entities.

---
+++ Vending.ApiLayer/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Vending.DtoLayer.Dtos.AppUserDto;
+using Vending.DtoLayer.Dtos.DepartmentsDto;
+using Vending.DtoLayer.Dtos.RoleDto;
+using Vending.DtoLayer.Dtos.TodoItemDto;
+using Vending.EntityLayer.Concrete;
+
+namespace Vending.ApiLayer.Mapping
+{
+    public class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<AppUser, LoginAppUserDto>().ReverseMap();
+            CreateMap<CreateAppUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginAppUserDto, AppUser>().ReverseMap();
+            CreateMap<CreateDepartmentDto, Department>().ReverseMap();
+            CreateMap<CreateTodoItemDto, TodoItem>().ReverseMap();
+            CreateMap<ResultDepartmentDto, Department>().ReverseMap();
+            CreateMap<UpdateRoleDto, AppRole>().ReverseMap();
+            CreateMap<ResultAppUserRolesDto, AppUser>().ReverseMap();
+            CreateMap<ResultAdminUserDto, AppUser>().ReverseMap();
+            CreateMap<ResultCustomerUserDto, AppUser>().ReverseMap();
+        }
+    }
+}
