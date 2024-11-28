b84ab8457331dbb15991367f15e4d7ab2d020727
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-11-28 23:30:30 +0300

    Add IAppUserService interface with user management methods
    Introduced a new interface `IAppUserService` in the `Vending.BusinessLayer.Abstract` namespace, extending `IGenericService<AppUser>`. Added method signatures for `RegisterUserAsync`, `LoginUserAsync`, `MobileLoginAsync`, and `MobileLogoutAsync`. Included necessary using directives for DTOs and response classes.

---
+++ Vending.BusinessLayer/Abstract/IAppUserService.cs
@@ -0,0 +1,14 @@
+﻿using Vending.DtoLayer.Dtos.AppUserDto;
+using Vending.DtoLayer.Response.AppUserResponse;
+using Vending.EntityLayer.Concrete;
+
+namespace Vending.BusinessLayer.Abstract
+{
+    public interface IAppUserService:IGenericService<AppUser>
+    {
+        Task<AppUserManagerResponse> RegisterUserAsync(CreateAppUserDto createAppUserDto);
+        Task<AppUserManagerResponse> LoginUserAsync(LoginAppUserDto loginAppUserDto);
+        Task<AppUserManagerResponse> MobileLoginAsync(LoginAppUserDto loginAppUserDto);
+        Task<AppUserManagerResponse> MobileLogoutAsync(string email);
+    }
+}
