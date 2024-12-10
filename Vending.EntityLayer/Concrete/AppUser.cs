f71ddd0fa25e29bd1822ac8bbbacd070814d4a07
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-12-10 20:36:10 +0300

    Add AppUser class with custom properties
    Introduce a new AppUser class inheriting from IdentityUser<int> in the Vending.EntityLayer.Concrete namespace. Added properties include FirstName, LastName, FullName, DepartmentID, and a navigation property Department to extend user information management.

---
+++ Vending.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Vending.EntityLayer.Concrete
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FullName{ get; set; }
+        public int? DepartmentID { get; set; }
+        public Department? Department { get; set; }
+    }
+}
