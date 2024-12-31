c21f83d90dda3d310136ad7b44bcae59c1848120
Ahmet Seyyit Köse <100533149+seyyitkse@users.noreply.github.com>
2024-12-31 23:57:46 +0300

    Add migration to update Products and Brands relationship
    Introduce _9_update_brand_product_relation migration.
    Add index on BrandId in Products table.
    Establish foreign key between Products and Brands with Restrict delete behavior.
    Provide Down method to reverse changes.

---
+++ Vending.DataAccessLayer/Migrations/20241222162416_9_update_brand_product_relation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vending.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _9_update_brand_product_relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Brands_BrandId",
+                table: "Products",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "BrandId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Brands_BrandId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_BrandId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Products");
+        }
+    }
+}
